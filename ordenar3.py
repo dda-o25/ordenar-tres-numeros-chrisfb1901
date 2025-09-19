2e1fbf6fbec201375bea75b8ebef966b33ed00e5
chrisfb1901 <christian.flores@iteso.mx>
2025-09-18 22:41:37 -0600

    mayormenor_chris

--- ordenar3.py
+++ ordenar3.py
@@ -1,15 +1,44 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Christian Alonso Flores Burgos
+
+Fecha
+19 de septiembre de 2025
+
+Dados tres números ordenarlos de mayor a menor.
+
 """
 
 # Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
-
+num1=int(input("Primer número:"))
+num2=int(input("Segundo número:"))
+num3=int(input("Tercer número:"))
 # Proceso
+if num1 >= num2 and num1 >= num3:
+    mayor = num1
+    if num2 >= num3:
+        medio = num2
+        menor = num3
+    else:
+        medio = num3
+        menor = num2
 
+elif num2 >= num1 and num2 >= num3:
+    mayor = num2
+    if num1 >= num3:
+        medio = num1
+        menor = num3
+    else:
+        medio = num3
+        menor = num1
 
+else: num3 >= num2 and num3 >= num1
+mayor = num3
+if num2 >= num1:
+        medio = num2
+        menor = num1
+else:
+        medio = num1
+        menor = num2
 # Salidas
-print(salida)
+print(mayor, medio, menor)
